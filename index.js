6577ceb5ee7c221f1a0560cdfc09e2258b818829
snowdreams1006 <snowdreams1006@163.com>
2020-03-27 01:50:32 +0800

    init plugin dir

---
+++ index.js
@@ -0,0 +1,28 @@
+/*
+ *  ===============================================================
+ *      snowdreams1006 is not just for programmers
+ *  ===============================================================
+ *
+ *  - Document: index.js
+ *  - Author: snowdreams1006
+ *  - Description: Gitbook plugin index
+ */
+
+module.exports = {
+    hooks: {
+        "page:before": function(page) {
+            if (this.options.pluginsConfig["punctuation-converter"]) {
+                page.content = page.content.replace(/,/g, ',')
+                .replace(/,/g, '，')
+                .replace(/./g, '。')
+                .replace(/:/g, '：')
+                .replace(/;/g, '；')
+                .replace(/!/g, '！')
+                .replace(/<</g, '《')
+                .replace(/>>/g, '》');
+
+                return page;
+            }
+        }
+    }
+};
